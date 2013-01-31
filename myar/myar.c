2e7226e21274da3bb1b654512a5385c62a28ee47
Keaton Wallace <wallacke@os-class.engr.oregonstate.edu>
2013-01-30 19:42:59 -0800

    Added a fair bunch of code from the book

--- myar/myar.c
+++ myar/myar.c
@@ -1,3 +1,7 @@
+/*copyfile, tlpi_hdr.h, get_num.h, error_functions.h,
+error_functions.c all come directly from the the book
+and I take no credit for it*/
+
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "tlpi_hdr.h"
@@ -5,6 +9,13 @@
 #define BUF_SIZE 1024
 #endif
 
+void errMsg(const char *format, ...);
+void errExit(const char *format, ...);
+void err_exit(const char *format, ...);
+void errExitEN(int errnum, const char *format, ...);
+void fatal(const char *format, ...);
+void usageErr(const char *format, ...);
+void cmdLineErr(const char *format, ...);
 
 void copyfile()
 {

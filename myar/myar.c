acb6d7ebf8f87a2caa7d608da2d0da6688f2dbb1
Keaton Wallace <wallacke@os-class.engr.oregonstate.edu>
2013-02-01 19:47:36 -0800

    Pulled together some general edits and started work on the metadata

--- myar/myar.c
+++ myar/myar.c
@@ -17,27 +17,29 @@ void fatal(const char *format, ...);
 void usageErr(const char *format, ...);
 void cmdLineErr(const char *format, ...);
 
-void append(argv*[])
+void append(char* argv[])
 {
 	int inputFd, outputFd, openFlags;
     mode_t filePerms;
-    ssize_t numRead;
+    ssize_t numRead=0;
     char buf[BUF_SIZE];
-    if (argc != 3 || strcmp(argv[1], "--help") == 0)
-	    usageErr("%s old-file new-file\n", argv[0]);
     /* Open input and output files */
-    inputFd = open(argv[1], O_RDONLY);
+    inputFd = open(argv[2], O_RDONLY);
     if (inputFd == -1)
-	    errExit("opening file %s", argv[1]);
-    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+	    errExit("opening file %s", argv[2]);
+    openFlags = O_CREAT | O_WRONLY | O_APPEND;
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
 	    S_IROTH | S_IWOTH; /* rw-rw-rw- */
-    outputFd = open(argv[2], openFlags, filePerms);
+    outputFd = open(argv[3], openFlags, filePerms);
     if (outputFd == -1)
-	    errExit("opening file %s", argv[2]);
+	    errExit("opening archive %s", argv[3]);
+    /*Write the metadata to the start of the file*/
+    char metaData[60];
+    char name[16] = argv[2];
+    //metaData = "%d", stat argv[2] -c %n;
     /* Transfer data until we encounter end of input or an error */
     while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
-	    if (write(outputFd, buf, numRead) != numRead)
+   	    if (write(outputFd, buf, numRead) != numRead)
 		    fatal("couldn't write whole buffer");
     if (numRead == -1)
 	    errExit("read");
@@ -46,14 +48,13 @@ void append(argv*[])
     if (close(outputFd) == -1)
 	    errExit("close output");
     exit(EXIT_SUCCESS);
-
 }
 
 int main(int argc, char *argv[])
 {
 	if(strcmp(argv[1], "-q")==0)
-		append(argv[]);
-	if(strcmp(argv[1], "-x")==0)
+		append(argv);
+	/*	if(strcmp(argv[1], "-x")==0)
 		extract();
 	if(strcmp(argv[1], "-t")==0)
 		concisetoc();
@@ -62,6 +63,6 @@ int main(int argc, char *argv[])
 	if(strcmp(argv[1], "-d")==0)
 		delete();
 	if(strcmp(argv[1], "-d")==0)
-		regular();
+	regular();*/
 	return 0;
 }

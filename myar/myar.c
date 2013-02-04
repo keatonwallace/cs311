24253f655fe6d28b34cc1a2fc71642cc6be37c04
Keaton Wallace <wallacke@os-class.engr.oregonstate.edu>
2013-02-04 15:26:03 -0800

    Print verbose and terse are now working.

--- myar/myar.c
+++ myar/myar.c
@@ -5,6 +5,7 @@ and I take no credit for it*/
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ar.h>
+#include <stdlib.h>
 #include "tlpi_hdr.h"
 #ifndef BUF_SIZE /* Allow "cc -D" to override definition */
 #define BUF_SIZE 1024
@@ -186,7 +187,7 @@ int loadhdr(int arFd, struct ar_hdr *hdr)
 	return 0;
 }
 /*based closely on the work by shuaiyuan.emory available at http://code.google.com/p/yuanshuai/source/browse/trunk/cs551/hw%231/myar.c?r=156*/
-/*static int print_table(int argc, char *argv[], int verbose)
+static int print_table(int argc, char *argv[], int verbose)
 {
 	int ar_fd;
 	int pos;
@@ -197,13 +198,11 @@ int loadhdr(int arFd, struct ar_hdr *hdr)
 	struct ar_hdr p_hdr;
 	struct stat arfileStat;
 	if((ar_fd=open(argv[2], O_RDONLY)) < 0)
-		fatal("can not open ar file:%s\n", argv[2]);
+		fatal("can not open ar file: %s\n", argv[2]);
 	if (stat (argv[2], &arfileStat)<0)
 		fatal("can not open ar file: %s\n", argv[2]);
-	pos =lseek(ar_fd,0,SEEK_CUR);
-	printf("%d\n", pos);
+	pos =lseek(ar_fd,8,SEEK_CUR);
 	int x = arfileStat.st_size;
-	printf("%d\n", x);
 	while (pos < arfileStat.st_size)
 	{
 		read(ar_fd, p_hdr.ar_name, sizeof(p_hdr.ar_name));
@@ -214,12 +213,11 @@ int loadhdr(int arFd, struct ar_hdr *hdr)
 		read(ar_fd, p_hdr.ar_size, sizeof(p_hdr.ar_size));
 		read(ar_fd, p_hdr.ar_fmag, sizeof(p_hdr.ar_fmag));
 		need_to_write = string2ll(p_hdr.ar_size, sizeof(p_hdr.ar_size));
-		//printf("%d\n", p_hdr.ar_size);
 		o_file_size = need_to_write;
 		t = index(p_hdr.ar_name, '/');
 		if (t<p_hdr.ar_date)
 			*t='\0';
-		/*if (verbose)
+		if (verbose == 0)
 		{
 			int tmp;
 			st_mode = string2int8(p_hdr.ar_mode, sizeof(p_hdr.ar_mode));
@@ -240,14 +238,13 @@ int loadhdr(int arFd, struct ar_hdr *hdr)
 			tmp = string2int(p_hdr.ar_size, sizeof(p_hdr.ar_size));
 			printf("%7d ", tmp);//size                                          
 			time_t file_time = (time_t)string2ll(p_hdr.ar_date, sizeof(p_hdr.ar_date));
-			printnf(ctime(&file_time)+4, 12);
+			printstr(ctime(&file_time)+4, 12);
 			printf(" ");
-			printnf(ctime(&file_time)+20, 4);
+			printstr(ctime(&file_time)+20, 4);
 			printf(" ");                                    
 			}
 		printf("%s\n", p_hdr.ar_name);                                  
 		pos =lseek(ar_fd,(o_file_size+1)&(~1),SEEK_CUR);
-		//printf("%d\n",pos);
 	}
 	close(ar_fd);
 	return 0;
@@ -259,19 +256,40 @@ static long long int string2ll(char *str, int str_len)
 	tmp[str_len]='\0';
 	return atoll(tmp);              
 }
-*/
+static int string2int(char *str, int str_len)
+{
+	char tmp[128];
+	memcpy(tmp, str, str_len);
+	tmp[str_len]='\0';
+	return atoi(tmp);               
+}
+static long  string2int8(char *str, int str_len)
+{
+	char tmp[128];
+	memcpy(tmp, str, str_len);
+	tmp[str_len]='\0';
+	return strtol(tmp, NULL, 8);
+                
+}
+void printstr(char *str, int len)
+{
+	char temp[128];
+	memcpy(temp, str, len);
+	temp[len]='\0';
+	printf(temp);
+}
 int main(int argc, char *argv[])
 {
-	//	if(strcmp(argv[1], "-w")==0)
-	//	print_table(argc, argv, 0);
-	if(strcmp(argv[1], "-q")==0)
+  	if(strcmp(argv[1], "-t")==0)
+		print_table(argc, argv, -1);
+	else if(strcmp(argv[1], "-q")==0)
 		append(argv);
 	else if(strcmp(argv[1], "-x")==0)
 		extract(argv);
-	else if(strcmp(argv[1], "-t")==0)
-		concisetoc(argv);
+	//else if(strcmp(argv[1], "-t")==0)
+	//	concisetoc(argv);
 	else if(strcmp(argv[1], "-v")==0)
-		verbosetoc(argv);
+		print_table(argc, argv, 0);
 	else if(strcmp(argv[1], "-d")==0)
 		delete(argv);
 	else if(strcmp(argv[1], "-A")==0)

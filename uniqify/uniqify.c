9fe73b397db763275d706eeb0fb520cfe5dc15fe
keatonwallace <wallacke@os-class.engr.oregonstate.edu>
2013-02-16 12:31:31 -0800

    Spawn should be done. Added support functions as needed. Added headers for incomplete functions.

--- uniqify/uniqify.c
+++ uniqify/uniqify.c
@@ -12,10 +12,13 @@
 //char* NULL
 //./a.out<in>out
 
+pid_t *children;
+int **to_children;
+int **to_nanny;
+
 /*spawn is a function that takes int as a  parameter and spawns that number of
  *child processes.
  */
-
 void spawn(int num_spawn, int **into, int **outof)
 {
 	pit_t pid;
@@ -23,27 +26,35 @@ void spawn(int num_spawn, int **into, int **outof)
 	for (x = 0; x < num_spawn; x++){
 		build_toilet(into[x]);
 		build_toilet(outof[x]);
-		switch((result = fork())){
+		switch((pid = fork())){
 		case -1:
 			//in parent, oops
 			perror("forking went awry (use a condom kids)");
 			break;
 		case 0:
 			//child case
-			close(into[i][1]);
-			//stopping here for now
-			FILE *output = fdopen(pipefds[1], "w");
-			fputs(buf, 10, output);
+			close(STDIN_FILENO);
+			close(STDOUT_FILENO);
+			dup2(into[x][0], STDIN_FILENO);
+			break_toilet(outof[x][0]);			      
+			dup2(outof[x][1], STDOUT_FILENO);
+			int i, j;
+			for (i = 0; i < x + 1; i++) {
+				for ( j = 0; j < 2; j++) {
+					break_toilet(into[i][j]);
+					break_toilet(outof[i][j]);
+				}
+			}
+			execlp("sort", "sort", (char *)NULL);
 			break;
 		default:
 			//parent case -- result holds pid of child
-			close(pipefds[1]);
-			FILE *input = fdopen(pipefds[0], "r");
-			fgets(buf, 10, input);
-
+			children[x] = pid;
+			break_toilet(into[x][0]);
+			break_toilet(outof[x][1]);
 			break;
-
-			}
+		}
+	}
 }
 
 void build_toilet(int *fdar)
@@ -52,35 +63,46 @@ void build_toilet(int *fdar)
 		perror("couldn't build a pipe");
 }
 
+void break_toilet(int fd)
+{
+	if(close(fd) == -1)
+		perror("couldn't close pipe");
+}
+
+int **build_bathroom(int num)
+{
+	int **toilets = malloc(sizeof(int *) * (num));
+	int x;
+	for (x = 0; x < num; x++){
+		toilets[x] = malloc(sizeof(int) * 2);
+	}
+	return toilets;
+}
+
+void destroy_bathroom(int num_toilets, int **bathroom)
+{
+}
+
+void hire_nanny(int **children)
+{
+}
+
+void nanny_disciplines(int **children)
+{
+}
+
 int main(int argc, char **argv)
 {
-	int num_childs = atoi(argv[1]);
-	int result;
-	int pipefds[2];
-	if(pipe(pipefds) != 0){
-		perror("Pipes are screwed up, call a plumber");
-		exit(-1);
-	}
-	for(int i = 0; i < num_of_proc; i++)
-	{
-		switch((result = fork())){
-		case -1:
-			//in parent, oops
-			break;
-		case 0:
-			//child case
-			close(pipefds[0]);
-			FILE *output = fdopen(pipefds[1], "w");
-			fputs(buf, 10, output);
-			break;
-		default:
-			//parent case -- result holds pid of child
-			close(pipefds[1]);
-		FILE *input = fdopen(pipefds[0], "r");
-		fgets(buf, 10, input);
-
-		break;
-	}
-	
+	int num_children = atoi(argv[1]);
+	to_children = build_bathroom(num_children);
+	to_nanny = build_bathroom(num_children);
+	spawn(num_children, to_children, to_nanny);
 	return 0;
 }
+
+/*todo
+ *figure out sig handler
+ *spawn suppressor
+ *choose/use supressor
+ *debug
+ */
